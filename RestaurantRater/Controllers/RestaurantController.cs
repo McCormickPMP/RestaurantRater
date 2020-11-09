7dce78bf04ec10ae4f803cee2e6a0055b46e155f
McCormickPMP <70542761+McCormickPMP@users.noreply.github.com>
2020-11-09 16:54:18 -0500

    Added Edit to Post

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -16,7 +17,7 @@ namespace RestaurantRater.Controllers
         {
             return View(_db.Restaurants.ToList());
         }
-        // Create: Restaurant/Create
+        // Get: Restaurant/Create
         public ActionResult Create()
         {
             return View();
@@ -58,5 +59,40 @@ namespace RestaurantRater.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+        //GET: Restaurant/Edit/{id}
+        //Get an id from the user
+        //Handle if the id is null
+        //Find a Restaurant by that id
+        //If the restaurant does not exist
+        //Return the restaurant and the view
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            return View(restaurant);
+        }
+
+        //POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+       
     }
+
 }
